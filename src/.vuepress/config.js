0bfdd5394197b58a8a61ce681fced4ffe5f59a72
Jonas Lannoo <72072084+JonasLannoo@users.noreply.github.com>
2021-11-22 16:39:43 +0100

    changed last global url link to ai@edge organisation

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -46,7 +46,7 @@ module.exports = {
       { text: 'IoT Incubator', link: 'https://iot-incubator.be/' },
       { text: 'EAVISE', link: 'https://iiw.kuleuven.be/onderzoek/eavise' }
     ],
-    repo: 'vives-ai-edge/workshop',
+    repo: 'vives-ai-edge',
     docsDir: 'src',
     docsBranch: 'master',
     sidebarDepth: 1,
