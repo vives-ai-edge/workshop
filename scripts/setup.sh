37521486ff03d82eaf1cccbe36984a23f6fa7a83
Jonas Lannoo <72072084+JonasLannoo@users.noreply.github.com>
2022-09-12 12:28:04 +0200

    updated to bullseye and tf2.8.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,6 +31,7 @@ echo "                                                                          
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt autoremove -y
 
 sudo apt install git -y
 
@@ -39,51 +40,67 @@ sudo apt install python3 python3-pip -y
 sudo -H pip3 install --upgrade pip
 
 # Install jupyter notebook
-sudo apt install libffi-dev -y
+# sudo apt install libffi-dev -y
 sudo -H pip3 install jupyter
-sudo -H pip3 install pygments==2.4.1
+# sudo -H pip3 install pygments==2.4.1
 
-if [ ! -f "/home/pi/.jupyter/jupyter_notebook_config.py" ]; then
-jupyter-notebook --generate-config
-grep -qxF 'c.NotebookApp.open_browser = False' /home/pi/.jupyter/jupyter_notebook_config.py || echo "c.NotebookApp.open_browser = False" >>  /home/pi/.jupyter/jupyter_notebook_config.py
-fi
+# if [ ! -f "/home/pi/.jupyter/jupyter_notebook_config.py" ]; then
+# jupyter-notebook --generate-config
+# grep -qxF 'c.NotebookApp.open_browser = False' /home/pi/.jupyter/jupyter_notebook_config.py || echo "c.NotebookApp.open_browser = False" >>  /home/pi/.jupyter/jupyter_notebook_config.py
+# fi
 
 # Make sure older versions are not installed. Only 2.4.1 is allowed
-if ! (pip3 list | grep "tensorflow.*2\.4\.1" > /dev/null); then
-  sudo pip uninstall tensorflow
-  sudo pip3 uninstall tensorflow
-fi
+# if ! (pip3 list | grep "tensorflow.*2\.4\.1" > /dev/null); then
+#   sudo pip uninstall tensorflow
+#   sudo pip3 uninstall tensorflow
+# fi
 
 # install the dependencies (if not already onboard)
-sudo apt-get install gfortran -y
-sudo apt-get install libhdf5-dev libc-ares-dev libeigen3-dev -y
-sudo apt-get install libatlas-base-dev libopenblas-dev libblas-dev -y
-sudo apt-get install liblapack-dev -y
+# sudo apt-get install gfortran -y
+# sudo apt-get install libhdf5-dev libc-ares-dev libeigen3-dev -y
+# sudo apt-get install libatlas-base-dev libopenblas-dev libblas-dev -y
+# sudo apt-get install liblapack-dev -y
 # upgrade setuptools 47.1.1 -> 50.3.2
-sudo -H pip3 install --upgrade setuptools
-sudo -H pip3 install pybind11
-sudo -H pip3 install Cython==0.29.21
+# sudo -H pip3 install --upgrade setuptools
+# sudo -H pip3 install pybind11
+# sudo -H pip3 install Cython==0.29.21
 # install h5py with Cython version 0.29.21 (± 6 min @1950 MHz)
-sudo -H pip3 install h5py==2.10.0
+# sudo -H pip3 install h5py==2.10.0
 # install gdown to download from Google drive
 
-sudo -H pip3 install numpy==1.19.5
-sudo -H pip3 install matplotlib pandas
+# sudo -H pip3 install numpy==1.19.5
+# sudo -H pip3 install matplotlib pandas
 
 # gdown https://drive.google.com/uc?id=1BLXP7RKEfTp9fxbmI8Qu2FdhU7NUxcwV
 
 # sudo -H pip3 install tensorflow-2.6.0-cp37-cp37m-linux_aarch64.whl
+# sudo -H pip3 install gdown
+
+sudo -H pip3 install protobuf==3.19.4
+
+# install tensorflow 2.7.0
+git clone -b v0.23.1 --depth=1 --recursive https://github.com/tensorflow/io.git
+cd io
+python3 setup.py -q bdist_wheel --project tensorflow_io_gcs_filesystem
+cd dist
+sudo -H pip3 install tensorflow_io_gcs_filesystem-0.23.1-cp39-cp39-linux_aarch64.whl
+cd ~/Documents
+# install gdown to download from Google drive
 sudo -H pip3 install gdown
+# download the wheel
+gdown https://drive.google.com/uc?id=1YpxNubmEL_4EgTrVMu-kYyzAbtyLis29
+# install TensorFlow 2.8.0
+sudo -H pip3 install tensorflow-2.8.0-cp39-cp39-linux_aarch64.whl
 
 source ~/.profile 
 
-if [ ! -f "./tensorflow-2.4.1-cp37-cp37m-linux_aarch64.whl" ]; then
-  gdown https://drive.google.com/uc?id=1WDG8Rbi0ph0sQ6TtD3ZGJdIN_WAnugLO
-fi
+# if [ ! -f "./tensorflow-2.4.1-cp37-cp37m-linux_aarch64.whl" ]; then
+#   gdown https://drive.google.com/uc?id=1WDG8Rbi0ph0sQ6TtD3ZGJdIN_WAnugLO
+# fi
 
 # install TensorFlow 2.4.1 (± 68 min @1950 MHz)
-sudo -H pip3 install wrapt --upgrade --ignore-installed # fix bug
-sudo -H pip3 install tensorflow-2.4.1-cp37-cp37m-linux_aarch64.whl
+# sudo -H pip3 install wrapt --upgrade --ignore-installed # fix bug
+# sudo -H pip3 install tensorflow-2.4.1-cp37-cp37m-linux_aarch64.whl
 
 # wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-aarch64.sh -O ~/miniconda.sh
 # bash ~/miniconda.sh -b -p $HOME/miniconda
@@ -125,7 +142,7 @@ sudo -H chmod +x /home/pi/Desktop/mbed-cli
 
 # Install nodejs
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo npm install -g npm
@@ -180,7 +197,7 @@ mbed target NUCLEO_L476RG
 mbed toolchain GCC_ARM
 
 # update for markupsafe version
-python -m pip install markupsafe==2.0.1
+# python -m pip install markupsafe==2.0.1
 
 echo "-----------------------------------------------------------"
 echo " Done !"
